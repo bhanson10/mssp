c8d40e06a6f6f92d7a5c70ba35c9850de1073d87
Benjamin Hanson <blhanson@ucsd.edu>
2025-02-12 10:01:47 -0800

    Simplified inputs

--- mssp.m
+++ mssp.m
@@ -1,38 +1,30 @@
-function [Sigma, Theta] = mssp(x, P, alpha, beta, kappa, n)
+function [Sigma, Theta] = mssp(x, P, alpha, beta, kappa)
 %MSSP Merwe Scaled Sigma Points
 %   [SIGMA, THETA] = MSSP() returns the Van der Merwe scaled sigma points SIGMA 
 %   and weights THETA for zero mean, identity covariance, spread alpha=1e-3, 
-%   prior knowledge beta=2, secondary scaling kappa=0, and number of pts n=3. 
+%   prior knowledge beta=2, secondary scaling kappa=0. 
 %
 %   [SIGMA, THETA] = MSSP(X) returns the Van der Merwe scaled sigma points 
 %   SIGMA and weights THETA for mean X, identity covariance, spread alpha=1e-3, 
 %   prior knowledge beta=2, and secondary scaling kappa=0. X is a D-by-1 
-%   vector, and number of pts n=2*D+1. 
+%   vector.
 %
 %   [SIGMA, THETA] = MSSP(X,P) returns the Van der Merwe scaled sigma 
 %   points SIGMA and weights THETA for mean X, covariance P, spread alpha=1e-3, 
 %   prior knowledge beta=2, and secondary scaling kappa=0. P is a
-%   D-by-D matrix, and number of pts n=2*D+1. 
+%   D-by-D matrix. 
 %
 %   [SIGMA, THETA] = MSSP(X,P,ALPHA) returns the Van der Merwe scaled 
 %   sigma points SIGMA and weights THETA for mean X, covariance P, spread
-%   ALPHA, prior knowledge beta=2, secondary scaling kappa=0, and number of 
-%   pts n=2*D+1. 
+%   ALPHA, prior knowledge beta=2, secondary scaling kappa=0.
 %
 %   [SIGMA, THETA] = MSSP(X,P,ALPHA,BETA) returns the Van der Merwe scaled 
 %   sigma points SIGMA and weights THETA for mean X, covariance P, spread
-%   ALPHA, prior knowledge BETA, secondary scaling kappa=0, and number of 
-%   pts n=2*D+1. 
+%   ALPHA, prior knowledge BETA, secondary scaling kappa=0.
 %
 %   [SIGMA, THETA] = MSSP(X,P,ALPHA,BETA,KAPPA) returns the Van der Merwe 
 %   scaled sigma points SIGMA and weights THETA for mean X, covariance P, 
-%   spread ALPHA, prior knowledge BETA, secondary scaling KAPPA, and number of 
-%   pts n=2*D+1. 
-%
-%   [SIGMA, THETA] = MSSP(X,P,ALPHA,BETA,KAPPA,N) returns the Van der Merwe 
-%   scaled sigma points SIGMA and weights THETA for mean X, covariance P, 
-%   spread ALPHA, prior knowledge BETA, secondary scaling KAPPA, and number of 
-%   pts N=2*N+1. 
+%   spread ALPHA, prior knowledge BETA, secondary scaling KAPPA.
 %
 %   SIGMA is an N-by-D matrix and THETA is a N-by-2 matrix where THETA(:,1) 
 %   are the mean weights and THETA(:,2) are the covariance weights.
@@ -59,8 +51,8 @@ function [Sigma, Theta] = mssp(x, P, alpha, beta, kappa, n)
 %      Inference in Dynamic State-Space Models" (Doctoral dissertation)
 
 if nargin < 1
-    x    = 0; 
-    P = 1; 
+    x     = 0; 
+    P     = 1; 
     alpha = 1e-3;
     beta  = 2;
     kappa = 0;
@@ -70,7 +62,7 @@ else
 end
 
 if nargin<2
-    P = eye(d); 
+    P     = eye(d); 
     alpha = 1e-3;
     beta  = 2;
     kappa = 0;
@@ -147,41 +139,6 @@ elseif nargin<6
         end
     end
     n     = 2*d+1; 
-elseif nargin<7
-    [d1_P, d2_P] = size(P);
-    if (d1_P ~= d)||(d2_P ~= d)
-        error("BadPDimension");
-    end
-    if ~isscalar(alpha)
-        error("BadAlphaDimension");
-    else
-        if (alpha < 0)||(alpha > 1)
-            error("BadAlpha");
-        end
-    end
-    if ~isscalar(beta)
-        error("BadBetaDimension");
-    else
-        if (beta < 0)
-            error("BadBeta");
-        end
-    end
-    if ~isscalar(kappa)
-        error("BadKappaDimension");
-    else
-        if (kappa < 0)
-            error("BadKappa");
-        end
-    end
-    if ~isscalar(n)
-        error("BadNDimension");
-    else
-        if (n < 0)
-            error("BadN");
-        else
-            n = 2*n + 1;
-        end
-    end
 else
     error("BadNumberOfInputs");
 end
@@ -192,12 +149,12 @@ Theta = c .* ones(n, 2);
 Theta(1,1) = lambda / (d + lambda); 
 Theta(1,2) = lambda / (d + lambda) + (1 - alpha^2 + beta); 
 
-U = chol((lambda + d) .* P);
+L = chol((lambda + d) .* P);
 Sigma = NaN(n,d);
 Sigma(1,:) = x';
 for i = 1:d
-    Sigma(i+1,:)   = x' + U(i,:); 
-    Sigma(d+i+1,:) = x' - U(i,:); 
+    Sigma(i+1,:)   = x' + L(i,:); 
+    Sigma(d+i+1,:) = x' - L(i,:); 
 end
 
 end
